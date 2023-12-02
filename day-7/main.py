2c4d466bced0a0069ba2a9e8c0024c3a2f01ca52
Maciej Zarembski <mackozb@gmail.com>
2023-12-02 01:41:10 +0100

    add rest of the days

---
+++ day-7/main.py
@@ -0,0 +1,21 @@
+
+def part_one(input_file):
+    with open(input_file, 'r') as data:
+        pass
+
+    return
+
+
+def part_two(input_file):
+    with open(input_file, 'r') as data:
+        pass
+
+    return
+
+
+if __name__ == '__main__':
+    input_file = 'input.txt'
+    results_file = 'results.txt'
+    with open(results_file, 'w') as results:
+        results.write('part 1 - {}\n'.format(part_one(input_file)))
+        results.write('part 2 - {}\n'.format(part_two(input_file)))

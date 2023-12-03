05a9d2b0073c0dbf4b6ddde147b628db9d848d9d
Maciej Zarembski <mackozb@gmail.com>
2023-12-03 08:20:27 +0100

    day 2, part 1

--- day-02/main.py
+++ day-02/main.py
@@ -1,9 +1,29 @@
 
 def part_one(input_file):
-    with open(input_file, 'r') as data:
-        pass
+    limits = {'red': 12, 'green': 13, 'blue': 14}
+    ids_sum = 0
 
-    return
+    with open(input_file, 'r') as data:
+        while True:
+            line = data.readline()
+            if not line:
+                break
+            line = line.rstrip('\n')
+            s = line.split(': ')
+            id = s[0].split(' ')[1]
+            limit_exceeded = False
+            for game in s[1].split('; '):
+                for count in game.split(', '):
+                    amount = count.split(' ')[0]
+                    color = count.split(' ')[1]
+                    if limits[color] < int(amount):
+                        limit_exceeded = True
+                        break
+                if limit_exceeded:
+                    break
+            if not limit_exceeded:
+                ids_sum += int(id)
+    return ids_sum
 
 
 def part_two(input_file):

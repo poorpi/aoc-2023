977a0c2e6039d5de2cf740cab40093a41cb4bafb
Maciej Zarembski <mackozb@gmail.com>
2023-12-05 16:35:01 +0100

    day 3, part 2

--- day-03/main.py
+++ day-03/main.py
@@ -65,13 +65,115 @@ def part_one(input_file):
 
 def part_two(input_file):
     with open(input_file, 'r') as data:
+        def check_line(lines):
+            def count_checks(checks):
+                hit_count = 0
+                if (checks[0][0] and checks[0][1] and checks[0][2]) or \
+                   (checks[0][0] and checks[0][1]) or \
+                   (checks[0][1] and checks[0][2]):
+                    hit_count += 1
+                    checks[0][0] = 0
+                    checks[0][2] = 0
+                elif checks[0][0] and checks[0][2]:
+                    hit_count += 2
+                elif checks[0][0] or checks[0][1] or checks[0][2]:
+                    hit_count += 1
+                hit_count += checks[1][0]
+                hit_count += checks[1][2]
+                if (checks[2][0] and checks[2][1] and checks[2][2]) or \
+                   (checks[2][0] and checks[2][1]) or \
+                   (checks[2][1] and checks[2][2]):
+                    hit_count += 1
+                    checks[2][0] = 0
+                    checks[2][2] = 0
+                elif checks[2][0] and checks[2][2]:
+                    hit_count += 2
+                elif checks[2][0] or checks[2][1] or checks[2][2]:
+                    hit_count += 1
+                return hit_count, checks
+
+            def get_number(line, index):
+                num = ''
+                while (index > 0 and line[index].isdigit()):
+                    index -= 1
+                if not line[index].isdigit():
+                    index += 1
+                while (index < len(line) and line[index].isdigit()):
+                    num += line[index]
+                    index += 1
+                return num
+
+            line_gear_ratios = 0
+            checks = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+            ]
+            for i, c in enumerate(lines[1]):
+                if c != '*':
+                    continue
+
+                checks = [
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0],
+                ]
+
+                if i > 0:
+                    if lines[0][i-1].isdigit():
+                        checks[0][0] = 1
+                    if lines[1][i-1].isdigit():
+                        checks[1][0] = 1
+                    if lines[2][i-1].isdigit():
+                        checks[2][0] = 1
+
+                if lines[0][i].isdigit():
+                    checks[0][1] = 1
+                if lines[2][i].isdigit():
+                    checks[2][1] = 1
+
+                if i < len(lines[1]) - 1:
+                    if lines[0][i+1].isdigit():
+                        checks[0][2] = 1
+                    if lines[1][i+1].isdigit():
+                        checks[1][2] = 1
+                    if lines[2][i+1].isdigit():
+                        checks[2][2] = 1
+
+                hit_count, checks = count_checks(checks)
+                nums = []
+                if hit_count != 2:
+                    continue
+
+                for ii in range(3):
+                    for jj in range(3):
+                        if checks[ii][jj]:
+                            nums.append(get_number(lines[ii], i + (jj - 2 + 1)))
+                line_gear_ratios += int(nums[0]) * int(nums[1])
+
+            return line_gear_ratios
+
+        lines = []
+        line = data.readline().rstrip('\n')
+        lines.append('.' * len(line))
+        lines.append('.' * len(line))
+        lines.append(line)
+
+        gear_rations_total = 0
         while True:
             line = data.readline()
             if not line:
                 break
             line = line.rstrip('\n')
+            lines.pop(0)
+            lines.append(line)
 
-    return
+            gear_rations_total += check_line(lines)
+
+        lines.pop(0)
+        lines.append('.' * len(lines[0]))
+        gear_rations_total += check_line(lines)
+    return gear_rations_total
 
 
 if __name__ == '__main__':

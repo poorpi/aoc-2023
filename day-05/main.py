17085170aa76ec85393ea9350830589724aafb49
Maciej Zarembski <mackozb@gmail.com>
2023-12-06 18:34:00 +0100

    day 5, part 1

--- day-05/main.py
+++ day-05/main.py
@@ -1,13 +1,64 @@
 
 def part_one(input_file):
+    def read_seeds(line):
+        seeds = []
+        line = line.split(': ')[1]
+        seeds = [int(x) for x in line.split(' ')]
+        return seeds
+
+    def read_map(destination, lines):
+        m = {'destination': destination, 'spans': []}
+        for line in lines:
+            line = [int(x) for x in line.split(' ')]
+            m['spans'].append({'destination_start': line[0], 'source_start': line[1], 'range': line[2]})
+        return m
+
     with open(input_file, 'r') as data:
+        line = data.readline().rstrip('\n')
+        seeds = read_seeds(line)
+        data.readline()
+
+        maps = {}
+        source = ''
+        destination = ''
+        lines = []
         while True:
             line = data.readline()
             if not line:
                 break
             line = line.rstrip('\n')
+            if not line:
+                if source and destination and lines:
+                    maps[source] = read_map(destination, lines)
+                    source = ''
+                    destination = ''
+                    lines = []
+                continue
+            if not source and not destination:
+                source = line.split('-to-')[0]
+                destination = line.split('-to-')[1].split(' ')[0]
+                continue
+            lines.append(line)
 
-    return
+        if source and destination and lines:
+            maps[source] = read_map(destination, lines)
+            source = ''
+            destination = ''
+            lines = []
+
+        locations = []
+        for seed_value in seeds:
+            current_value = seed_value
+            current_map = 'seed'
+            while current_map != 'location':
+                for span in maps[current_map]['spans']:
+                    if current_value >= span['source_start'] and current_value < span['source_start'] + span['range']:
+                        current_value = span['destination_start'] + current_value - span['source_start']
+                        break
+                current_map = maps[current_map]['destination']
+            locations.append(current_value)
+
+    return min(locations)
 
 
 def part_two(input_file):

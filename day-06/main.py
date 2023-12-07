47e600367cd0d8ff8fd33e72ad3974a20f8c65cb
Maciej Zarembski <mackozb@gmail.com>
2023-12-07 18:10:45 +0100

    day 6, part 1

--- day-06/main.py
+++ day-06/main.py
@@ -1,13 +1,20 @@
 
 def part_one(input_file):
     with open(input_file, 'r') as data:
-        while True:
-            line = data.readline()
-            if not line:
-                break
-            line = line.rstrip('\n')
+        def distance(time, speed):
+            return time * speed
 
-    return
+        times = [int(x) for x in data.readline().split(':')[1].split()]
+        distances = [int(x) for x in data.readline().split(':')[1].split()]
+
+        total_wins_mul = 1
+        for race in range(len(times)):
+            num_wins = 0
+            for i in range(times[race]):
+                if distance(times[race] - i, i) > distances[race]:
+                    num_wins += 1
+            total_wins_mul *= num_wins
+    return total_wins_mul
 
 
 def part_two(input_file):

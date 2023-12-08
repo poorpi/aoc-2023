2e1a2defa6063ddf705caeb327af4f6d26e05715
Maciej Zarembski <mackozb@gmail.com>
2023-12-08 12:17:06 +0100

    day 8, part 2

--- day-08/main.py
+++ day-08/main.py
@@ -29,13 +29,42 @@ def part_one(input_file):
 
 def part_two(input_file):
     with open(input_file, 'r') as data:
+        instructions = data.readline().rstrip('\n')
+        data.readline()
+        nodes = {}
         while True:
             line = data.readline()
             if not line:
                 break
             line = line.rstrip('\n')
-
-    return
+            node_name = line.split(' = ')[0]
+            directions = line.split(' = ')[1].strip('()')
+            nodes[node_name] = {'name': node_name, 'L': directions.split(', ')[0], 'R': directions.split(', ')[1]}
+    
+    end_reached = False
+    end_node_suffix = 'Z'
+    solvers = []
+    for k, v in nodes.items():
+        if k[-1] == 'A':
+            solvers.append({'node': v, 'end_steps': 0})
+    steps = 0
+    finished_solvers = 0
+    while not end_reached:
+        for instruction in instructions:
+            steps += 1
+            for i in range(len(solvers)):
+                if solvers[i]['end_steps']:
+                    continue
+                solvers[i]['node'] = nodes[solvers[i]['node'][instruction]]
+                if solvers[i]['node']['name'][-1] == end_node_suffix:
+                    solvers[i]['end_steps'] = steps
+                    finished_solvers += 1
+            if finished_solvers == len(solvers):
+                end_reached = True
+                break
+    from math import lcm
+    result = lcm(*[x['end_steps'] for x in solvers])
+    return result
 
 
 if __name__ == '__main__':
@@ -44,3 +73,4 @@ if __name__ == '__main__':
     with open(results_file, 'w') as results:
         results.write('part 1 - {}\n'.format(part_one(input_file)))
         results.write('part 2 - {}\n'.format(part_two(input_file)))
+ 

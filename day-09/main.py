24582df0e92172fc5b5f837cbc3f6a7e988a6cee
Maciej Zarembski <mackozb@gmail.com>
2023-12-09 10:05:29 +0100

    day 9, part 1

--- day-09/main.py
+++ day-09/main.py
@@ -1,13 +1,34 @@
 
 def part_one(input_file):
+    extrapolated_sum = 0
     with open(input_file, 'r') as data:
         while True:
             line = data.readline()
             if not line:
                 break
             line = line.rstrip('\n')
+            numbers = [int(x) for x in line.split()]
+            stage_lasts = []
+            stage_lasts.append(numbers[-1])
+            current_numbers = numbers
+            while True:
+                all_zeros = True
+                new_numbers = []
+                for ix in range(len(current_numbers) - 1):
+                    new_numbers.append(current_numbers[ix + 1] - current_numbers[ix])
+                    if new_numbers[-1]:
+                        all_zeros = False
+                if all_zeros:
+                    break
+                stage_lasts.append(new_numbers[-1])
+                current_numbers = new_numbers
+            stage_lasts.reverse()
+            extrapolated = 0
+            for stage_last in stage_lasts:
+                extrapolated = extrapolated + stage_last
+            extrapolated_sum += extrapolated
 
-    return
+    return extrapolated_sum
 
 
 def part_two(input_file):
